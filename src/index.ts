c813f559d627f5341181027aa04661456eeddcbf
byunghyun <eer3481@naver.com>
2025-07-16 14:32:23 +0900

    feature: 초성 검색 오류 해결

--- src/index.ts
+++ src/index.ts
@@ -45,7 +45,7 @@ export const fullTextSearch = <T extends { [Key: string]: any }>({
   });
 };
 
-// 핵심 개선된 부분
+// 핵심 로직
 const fullTextSearchCore = <T extends { [Key: string]: any }>(
   dataItem: T,
   searchRequirement: Array<{
@@ -66,70 +66,75 @@ const fullTextSearchCore = <T extends { [Key: string]: any }>(
       : value;
 
     const isKorean = /[ㄱ-ㅎ가-힣]/.test(searchFilterText);
+
     if (isKorean) {
-      const initials = getInitials(convertedText);
-      return initials.includes(searchFilterText);
+      const initialsList = getInitialsList(convertedText); // ✨ 단어별 초성 리스트
+      return initialsList.some((initials) =>
+        initials.includes(searchFilterText),
+      );
     }
 
     return new RegExp(escapeRegExp(searchFilterText), 'i').test(convertedText);
   });
 };
 
-// 초성만 추출하는 로직
-const getInitials = (text: string): string => {
-  const initials = [];
-
-  for (let char of text) {
-    const code = char.charCodeAt(0);
-    if (code >= 0xac00 && code <= 0xd7a3) {
-      const cho = Math.floor((code - 0xac00) / 588);
-      initials.push('ㄱㄲㄴㄷㄸㄹㅁㅂㅃㅅㅆㅇㅈㅉㅊㅋㅌㅍㅎ'[cho]);
-    }
-  }
-
-  return initials.join('');
+// ✅ 단어별 초성만 추출한 배열 리턴
+const getInitialsList = (text: string): string[] => {
+  return text
+    .split(/\s+/) // 띄어쓰기 기준
+    .map((word) => {
+      const initials = [];
+      for (let char of word) {
+        const code = char.charCodeAt(0);
+        if (code >= 0xac00 && code <= 0xd7a3) {
+          const cho = Math.floor((code - 0xac00) / 588);
+          initials.push('ㄱㄲㄴㄷㄸㄹㅁㅂㅃㅅㅆㅇㅈㅉㅊㅋㅌㅍㅎ'[cho]);
+        }
+      }
+      return initials.join('');
+    });
 };
 
 const escapeRegExp = (string: string) => {
   return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
 };
 
-// 이하 함수들은 유지 (미사용 가능성 있음)
-// const charCodeAts: { [Key in string]: number } = {
-//   ㄱ: '가'.charCodeAt(0),
-//   ㄲ: '까'.charCodeAt(0),
-//   ㄴ: '나'.charCodeAt(0),
-//   ㄷ: '다'.charCodeAt(0),
-//   ㄸ: '따'.charCodeAt(0),
-//   ㄹ: '라'.charCodeAt(0),
-//   ㅁ: '마'.charCodeAt(0),
-//   ㅂ: '바'.charCodeAt(0),
-//   ㅃ: '빠'.charCodeAt(0),
-//   ㅅ: '사'.charCodeAt(0),
-// } as const;
+const charCodeAts: { [Key in string]: number } = {
+  ㄱ: '가'.charCodeAt(0),
+  ㄲ: '까'.charCodeAt(0),
+  ㄴ: '나'.charCodeAt(0),
+  ㄷ: '다'.charCodeAt(0),
+  ㄸ: '따'.charCodeAt(0),
+  ㄹ: '라'.charCodeAt(0),
+  ㅁ: '마'.charCodeAt(0),
+  ㅂ: '바'.charCodeAt(0),
+  ㅃ: '빠'.charCodeAt(0),
+  ㅅ: '사'.charCodeAt(0),
+} as const;
 
-// const characterPatternCheckerCore = (character: string) => {
-//   const offset = 44032;
-//   if (/[가-힣]/.test(character)) {
-//     const chCode = character.charCodeAt(0) - offset;
-//     if (chCode % 28 > 0) {
-//       return character;
-//     }
-//     const begin = Math.floor(chCode / 28) * 28 + offset;
-//     const end = begin + 27;
-//     return `[\\u${begin.toString(16)}-\\u${end.toString(16)}]`;
-//   }
-//   if (/[ㄱ-ㅎ]/.test(character)) {
-//     const begin =
-//       charCodeAts[character] ||
-//       (character.charCodeAt(0) - 12613) * 588 + charCodeAts['ㅅ'];
-//     const end = begin + 587;
+const characterPatternCheckerCore = (character: string) => {
+  const offset = 44032;
+  if (/[가-힣]/.test(character)) {
+    const chCode = character.charCodeAt(0) - offset;
+    if (chCode % 28 > 0) {
+      return character;
+    }
+    const begin = Math.floor(chCode / 28) * 28 + offset;
+    const end = begin + 27;
+    return `[\\u${begin.toString(16)}-\\u${end.toString(16)}]`;
+  }
+  if (/[ㄱ-ㅎ]/.test(character)) {
+    const begin =
+      charCodeAts[character] ||
+      (character.charCodeAt(0) - 12613) * 588 + charCodeAts['ㅅ'];
+    const end = begin + 587;
 
-//     return `[${character}\\u${begin.toString(16)}-\\u${end.toString(16)}]`;
-//   }
-//   return escapeRegExp(character);
-// };
+    return `[${character}\\u${begin.toString(16)}-\\u${end.toString(16)}]`;
+  }
+  return escapeRegExp(character);
+};
 
+// 아래 함수들은 현재 사용되지 않음 (옵션 확장용으로만 유지)
 // const createInitialLetterMatcher = (searchText: string) => {
 //   const pattern = searchText
 //     .split('')
